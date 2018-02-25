035d244ae078501ede86fcbb39c442f35c6672f4
Justine Tran <justinetran@JustineMac.local>
2018-02-25 14:09:59 -0800

    Added previous and next buttons

--- scripts/main.js
+++ scripts/main.js
@@ -42,10 +42,88 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+//Commented out code: There is a glitch if thumbnail is clicked before prev/next buttons
+// function findIndex(thumbnailArray, imgName, property) {
+//   "use strict";
+//   for(var i = 0; i < thumbnailArray.length; i++) {
+//     if(thumbnailArray[i][property] === imgName) return i;
+//   }
+//   return -1;
+// }
+//
+// function buttonClickHandler() {
+//   "use strict";
+//   var thumbnailArray = getThumbnailsArray();
+//   var thumbnailArrayLen = thumbnailArray.length;
+//   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+//   var curIndex = findIndex(thumbnailArray, detailImage.src, "href");
+//
+//   document.getElementById("prev-button").addEventListener("click", function(event) {
+//     event.preventDefault();
+//
+//     curIndex-=1;
+//
+//     if(curIndex < 0) { //wrap around if index becomes negative
+//       curIndex += thumbnailArrayLen;
+//     }
+//     curIndex = curIndex % thumbnailArrayLen;
+//
+//     setDetailsFromThumb(thumbnailArray[curIndex]);
+//   });
+//
+//   document.getElementById("next-button").addEventListener("click", function(event) {
+//     event.preventDefault();
+//
+//     curIndex = (curIndex+1) % thumbnailArrayLen; //wrap around if index is out of bounds
+//
+//     setDetailsFromThumb(thumbnailArray[curIndex]);
+//   });
+// }
+
 function initializeEvents() {
   "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  // buttonClickHandler();
 }
 
 initializeEvents();
+
+function findIndex(thumbnailArray, imgName, property) {
+  "use strict";
+  for (var i = 0; i < thumbnailArray.length; i++) {
+    if (thumbnailArray[i][property] === imgName) return i;
+  }
+  return -1;
+}
+
+document.getElementById("prev-button").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var thumbnailArray = getThumbnailsArray();
+  var thumbnailArrayLen = thumbnailArray.length;
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var curIndex = findIndex(thumbnailArray, detailImage.src, "href");
+
+  curIndex -= 1;
+
+  if (curIndex < 0) { //wrap around if index becomes negative
+    curIndex += thumbnailArrayLen;
+  }
+  curIndex = curIndex % thumbnailArrayLen;
+
+  setDetailsFromThumb(thumbnailArray[curIndex]);
+});
+
+document.getElementById("next-button").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var thumbnailArray = getThumbnailsArray();
+  var thumbnailArrayLen = thumbnailArray.length;
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var curIndex = findIndex(thumbnailArray, detailImage.src, "href");
+
+  curIndex = (curIndex + 1) % thumbnailArrayLen; //wrap around if index is out of bounds
+
+  setDetailsFromThumb(thumbnailArray[curIndex]);
+});
